38e1cd3ae1e8a4cd16ddfee6ce6fbe1b08350bc9
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2022-04-28 17:26:14 -0300

    Setting blank property for not required fields

--- core/models/edital.py
+++ core/models/edital.py
@@ -8,7 +8,7 @@ class Edital(models.Model):
     titulo_longo = models.CharField(max_length=255)
     titulo_curto = models.CharField(max_length=16)
     vigente = models.BooleanField()
-    url = models.URLField()
+    url = models.URLField(blank=True)
 
     def __str__(self):
         return self.titulo_longo
--- core/models/servidor.py
+++ core/models/servidor.py
@@ -5,11 +5,12 @@ class Servidor(models.Model):
         verbose_name_plural = "servidores"
     
     nome = models.CharField(max_length=128)
-    cpf = models.CharField(max_length=16)
-    siape = models.CharField(max_length=16)
+    cpf = models.CharField(max_length=16, blank=True)
+    siape = models.CharField(max_length=16, blank=True)
     email = models.EmailField()
     campus = models.CharField(max_length=32)
-    area_qualis = models.CharField(max_length=64)
+    area_qualis = models.CharField(max_length=64, blank=True)
+    link_lattes = models.URLField(blank=True)
 
     def __str__(self):
         return self.nome
